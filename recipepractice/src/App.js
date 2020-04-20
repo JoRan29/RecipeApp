2d94c5e3a36e3ecf7f9ea513c5ae620a4f0389f1
JoRan29 <jokavana29@gmail.com>
2020-04-20 18:50:51 +0200

    fetch repices from api

--- recipepractice/src/App.js
+++ recipepractice/src/App.js
@@ -1,17 +1,29 @@
-import React from "react";
-import logo from "./logo.svg";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 
 const App = () => {
   const APP_ID = "1bc03387";
   const APP_KEY = "5ad9bfbd59e521055b37120a7a325bc2";
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+
+  useEffect(() => {
+    console.log("Effect has been run!");
+  }, []);
+
+  const getRecipes = async () => {
+    const response = await fetch(
+      `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`
+    );
+    const data = response.json();
+    console.log(data);
+  };
 
   return (
     <div className="App">
-      <form action="">
-        <input type="text" />
-        <button type="submit"></button>
+      <form action="" className="search-form">
+        <input type="text" className="search-bar" />
+        <button type="submit" className="search-btn">
+          Search
+        </button>
       </form>
     </div>
   );
